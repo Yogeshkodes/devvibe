d5e2533d354b1e2f4c9dabc33055d1512644b148
Yogeshkodes <yogesh.cr712@gmail.com>
2025-08-13 23:27:23 +0530

    Template selection modal added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,37 +1,36 @@
-import AddNewButton from "@/features/dashboard/components/add-new-btn";
-import AddRepo from "@/features/dashboard/components/add-repo";
-
-import ProjectTable from "@/features/dashboard/components/project-table";
+import EmptyState from "@/components/ui/empty-state";
 import {
-  getAllPlaygroundForUser,
   deleteProjectById,
-  editProjectById,
   duplicateProjectById,
-} from "@/features/playground/actions";
+  editProjectById,
+  getAllPlaygroundForUser,
+} from "@/features/dashboard/actions";
+import AddNewButton from "@/features/dashboard/components/add-new-button";
+import AddRepoButton from "@/features/dashboard/components/add-repo-button";
+import ProjectTable from "@/features/dashboard/components/project-table";
+import React from "react";
 
-const EmptyState = () => (
-  <div className="flex flex-col items-center justify-center py-16">
-    <img src="/empty-state.svg" alt="No projects" className="w-48 h-48 mb-4" />
-    <h2 className="text-xl font-semibold text-gray-500">No projects found</h2>
-    <p className="text-gray-400">Create a new project to get started!</p>
-  </div>
-);
-
-const DashboardMainPage = async () => {
+const Page = async () => {
   const playgrounds = await getAllPlaygroundForUser();
-  console.log(playgrounds);
   return (
     <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
         <AddNewButton />
-        <AddRepo />
+        <AddRepoButton />
       </div>
+
       <div className="mt-10 flex flex-col justify-center items-center w-full">
         {playgrounds && playgrounds.length === 0 ? (
-          <EmptyState />
+          <EmptyState
+            title="No projects Found"
+            description="Create a new Project to get started"
+            imageSrc="/empty-state.svg"
+          />
         ) : (
-          // @ts-ignore
+          // todo add playground table
           <ProjectTable
+            // @ts-ignore
+            // TODO: NEED TO UPDATE THE TYPES OF THE PLAYGROUND
             projects={playgrounds || []}
             onDeleteProject={deleteProjectById}
             onUpdateProject={editProjectById}
@@ -43,4 +42,4 @@ const DashboardMainPage = async () => {
   );
 };
 
-export default DashboardMainPage;
+export default Page;
